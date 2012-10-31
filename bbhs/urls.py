c7a933b29d1053ea19512d7c64a64b10164f151e
Luis Naranjo <luis@luis-ubuntu-techta.(none)>
2012-10-31 09:35:23 -0700

    Added User extension profile

--- bbhs/urls.py
+++ bbhs/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'bbhs.views.index'),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^chaperone/', include('chaperone.urls'),),
     url(r'^service/', include('service.urls'),),
 )
