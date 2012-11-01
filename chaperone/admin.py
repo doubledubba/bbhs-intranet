5ed5f3d692621e99d17de07d2a0e584399e476d7
luis <luis@luis-mint-desktop.(none)>
2012-10-31 19:25:49 -0700

    Added event tables

--- chaperone/admin.py
+++ chaperone/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from chaperone.models import UserProfile
+from chaperone.models import UserProfile, Event
 
 # Define an inline admin descriptor for UserProfile model
 # which acts a bit like a singleton
@@ -21,4 +21,4 @@ class UserAdmin(UserAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-
+admin.site.register(Event)
--- chaperone/models.py
+++ chaperone/models.py
@@ -6,10 +6,28 @@ from django.db.models.signals import post_save
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
 
-    events = models.IntegerField()
+    events = models.IntegerField(null=True)
 
     def __unicode__(self):
         return self.user.username
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=80)
+    admin = models.ForeignKey(UserProfile)
+    date = models.DateTimeField()
+    volunteersNeeded = models.IntegerField()
+    volunteersRegistered = models.IntegerField()
+    description = models.TextField(blank=True)
+
+    volunteersNeeded.verbose_name = 'Volunteers Needed'
+    volunteersRegistered.verbose_name = 'Volunteers Registered'
+
+    def __unicode__(self):
+        return '%s on %s with %s' % (self.name, self.date.strftime('%c'),
+                self.admin)
+
+
 
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
