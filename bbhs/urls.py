59c5bf288d92c289974e463691c75dbd942dcfe0
luis <luis@luis-mint-desktop.(none)>
2012-10-31 20:01:49 -0700

    Auth is working

--- bbhs/urls.py
+++ bbhs/urls.py
@@ -3,8 +3,16 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', 'bbhs.views.index'),
+from django.contrib.auth.views import logout
+
+urlpatterns = patterns('bbhs.views',
+    url(r'^$', 'index'),
+    url(r'^login/$', 'loginView'),
+    url(r'^logout/$', 'logoutView'),
+
+)
+
+urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^chaperone/', include('chaperone.urls'),),
     url(r'^service/', include('service.urls'),),
--- bbhs/views.py
+++ bbhs/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django import forms
+from django.contrib.auth import authenticate, login, logout
+
 
 def index(request):
     return render(request, 'index.html')
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput)
+
+
+def loginView(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = LoginForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/') # Redirect after POST
+                else:
+                    return HttpResponse('Your account has been disabled!')
+            else:
+                return HttpResponse('Your username and password were wrong')
+
+    else:
+        form = LoginForm() # An unbound form
+
+    return render(request, 'login.html', {
+        'form': form,
+    })
+
+def logoutView(request):
+    logout(request)
+    return render(request, 'message.html', {'msg': 'Logged out!'})
+
