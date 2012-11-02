718c6066fbe14adba71e40032814600bbc81f9b0
Luis Naranjo <luis@luis-ubuntu-techta.(none)>
2012-11-02 11:00:58 -0700

    IDK WHAT I DID

--- chaperone/models.py
+++ chaperone/models.py
@@ -43,7 +43,8 @@ class Event(models.Model):
             self.volunteersRegistered = volunteer.pk
         else:
             self.volunteersRegistered += ',%d' % volunteer.pk
-        volunteer.eventsNeeded += 0
+        volunteer.eventsNeeded -= 0
+        self.volunteersNeeded -= 0
         self.save()
         print 'Added:', volunteer
 
