b144d2772760c9334bf8f8cc9c0d3070dd766564
Luis Naranjo <luisnaranjo733@gmail.com>
2012-11-06 10:42:09 -0800

    Added app specific URL and view framework
    All functional

--- bbhs/settings.py
+++ bbhs/settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
 
+    'intranet',
     'chaperone',
     'service',
 )
--- bbhs/urls.py
+++ bbhs/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('bbhs.views',
+urlpatterns = patterns('intranet.views',
     url(r'^$', 'index'),
     url(r'^login/$', 'loginView'),
     url(r'^logout/$', 'logoutView'),
