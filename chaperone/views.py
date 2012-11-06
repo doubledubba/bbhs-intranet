2232496c92106b979b3025739f209f7094482cfa
Luis Naranjo <luisnaranjo733@gmail.com>
2012-11-06 10:37:23 -0800

    Hard reset.
    Re-created chaperone and service app - they are clean.
    Creating a global intranet app, literally called intranet
    for shared information between the apps.
    Starting from scratch to avoid confusion

--- chaperone/views.py
+++ chaperone/views.py
@@ -1,11 +1,1 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from chaperone.models import Event, UserProfile
-
-@login_required
-def index(request):
-    params = {'events': Event.future_events()}
-    return render(request, 'chaperone/index.html', params)
-
-
+# Create your views here.
