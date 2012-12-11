5503fd624f270610618fbd3effa9025ba32eafde
Luis Naranjo <luisnaranjo733@gmail.com>
2012-12-11 10:55:02 -0800

    Removed old import that's obsolete

--- chaperone/views.py
+++ chaperone/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-#from chaperone.models import Event, UserProfile
 
 @login_required
 def index(request):
