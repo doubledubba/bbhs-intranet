b94075aa99e0a85ddacd3898f94ea35758e58809
Luis Naranjo <luisnaranjo733@gmail.com>
2013-06-06 11:05:28 -0700

    Added a really cool admin actions for bulk toggling event description markdown rendering

--- chaperone/admin.py
+++ chaperone/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from chaperone.models import Event
 
-admin.site.register(Event)
+def turnOnRender(modelAdmin, request, querySet):
+    querySet.update(markdown=True)
+
+def turnOffRender(modelAdmin, request, querySet):
+    querySet.update(markdown=False)
+
+turnOnRender.short_description = 'Turn on the markdown rendering feature'
+turnOffRender.short_description = 'Turn off the markdown rendering feature'
+
+class EventAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+    actions = [turnOnRender, turnOffRender]
+
+admin.site.register(Event, EventAdmin)
 
