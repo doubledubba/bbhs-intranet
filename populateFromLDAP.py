a700b0dde8f7db33330aee430e05b205e036c8be
Luis Naranjo <luisnaranjo733@gmail.com>
2013-06-13 11:39:09 -0700

    Now skipping disabled users

---
+++ populateFromLDAP.py
@@ -0,0 +1,83 @@
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'bbhs.settings'
+from tempfile import NamedTemporaryFile as temp
+from ldif import LDIFParser
+from django.contrib.auth.models import User
+from bbhs.settings import PROJECT_ROOT, AUTH_LDAP_SERVER_URI
+from bbhs.settings import AUTH_LDAP_BIND_PASSWORD, AUTH_LDAP_BIND_DN, faculty_cn
+
+fh = temp()
+command = 'ldapsearch -a always -H %s -w %s -D "%s" -b "%s" "sAMAccountName=*" -u -s sub sAMAccountName mail givenName sn userAccountControl> %s'
+command = command % (AUTH_LDAP_SERVER_URI, AUTH_LDAP_BIND_PASSWORD, AUTH_LDAP_BIND_DN, faculty_cn, fh.name)
+os.system(command)
+
+deList = lambda obj: obj[0] if obj else ''
+
+'''
+User Account Control States
+
+66080 --> Password not required | Normal account | Don't expire
+66082 --> Account disable | Password not required | Normal account
+512 --> Normal Account
+66048 --> Normal acccount | Don't expire password
+66050 --> Account disable | normal | no expire password
+514 --> Account disable | Normal account
+
+Disabled = ['66082', '66050', '514']
+'''
+
+class MyLDIF(LDIFParser):
+    def __init__(self,input):
+        LDIFParser.__init__(self,input)
+        self.users = []
+
+    def handle(self, dn, _entry):
+        username = _entry.get('sAMAccountName')
+        if not username:
+            return
+        email = _entry.get('mail')
+        first_name = _entry.get('givenName')
+        last_name = _entry.get('sn')
+        UAC = _entry.get('userAccountControl')
+        
+                
+        username = deList(username)
+        email = deList(email)
+        first_name = deList(first_name)
+        last_name = deList(last_name)
+        UAC = deList(UAC)
+        
+        if UAC in ['66082', '66050', '514']:
+            print 'Skipping disabled user:', username
+            return
+        
+        user = {'username': username, 'email': email, 
+            'first_name': first_name, 'last_name': last_name}
+        self.users.append(user)
+
+fh.seek(0)  
+parser = MyLDIF(fh)
+parser.parse()
+users = parser.users
+fh.close()
+del parser, fh, os, temp, LDIFParser
+
+for user in users:
+    username = user.get('username')
+    email = user.get('email')
+    first_name = user.get('first_name')
+    last_name = user.get('last_name')
+    
+    try:
+        user = User.objects.get(username=username)
+        print username, 'already exists!'
+    except User.DoesNotExist:
+        user = User()
+        user.username = username
+        user.email = email
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        print 'Created:', username
+    
+
