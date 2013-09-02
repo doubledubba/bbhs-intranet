8681ade0133bbc820faef7b588548c446957da6c
Luis Naranjo <luisnaranjo733@gmail.com>
2013-09-02 08:55:13 -0700

    changes

--- hours.py
+++ hours.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 
-import os
+import os, csv
+from dateutil import parser
+
 
 command = 'git log --pretty=format:"%ad|%x09%s" > log.txt'
 os.system(command)
+
+commits = []
+
+with open('log.txt', 'rb') as fh:
+    reader = csv.reader(fh, delimiter='|')
+    for timestamp, cm in reader:
+        timestamp = parser.parse(timestamp)
+        cm = cm[1:]
+        commits.append((timestamp, cm))
+
+commits.reverse()
+
+# First item in the list is first commit
+# All commits in chronological order
+
