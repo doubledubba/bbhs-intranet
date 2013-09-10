a07fec1409d8f5a30f1b439ee0d62d146560862e
Luis Naranjo <luisnaranjo733@gmail.com>
2013-09-09 21:26:49 -0700

    wassup hours are fixed

--- hours.py
+++ hours.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import os, csv
+from collections import OrderedDict
 from dateutil import parser
+from pprint import pprint
 
 
-command = 'git log --pretty=format:"%ad|%x09%s" > log.txt'
+command = 'git log --pretty=format:"%ad|%x09%s" --since="2013-06-07" > log.txt'
+
 os.system(command)
 
 commits = []
@@ -21,3 +24,62 @@ commits.reverse()
 # First item in the list is first commit
 # All commits in chronological order
 
+
+dates = OrderedDict()
+for commit in commits:
+    timestamp, message = commit
+    date = timestamp.date()
+    if not date in dates:
+        dates[date] = [commit,]
+    else:
+        dates[date].append(commit)
+
+def getInt(msg):
+    initial = raw_input(msg)
+    while True:
+        if initial.isdigit():
+            break
+        else:
+            initial = raw_input(msg)
+    return int(initial)
+
+
+hours = 0
+
+mode = raw_input('Manual or automatic counting?: ')
+if mode.lower() == 'manual':
+    manual = True
+elif mode.lower() == 'automatic':
+    manual = False
+else:
+    print 'Error! Input either "manual" or "automatic"'
+    exit(1)
+
+for date in dates:
+    print '%d hours so far' % hours
+    print date.strftime('%A %B %d')
+    print '=' * len(str(date))
+
+    start = dates[date][0][0]
+    end = dates[date][-1][0]
+    delta = (end-start).seconds/60.0/60.0
+    print '%s - %s = %r hours\n' % (end.strftime('%H:%M %p'), start.strftime('%H:%M %p'), round(delta, 2))
+
+    for commit in dates[date]:
+        timestamp, message = commit
+        print timestamp.strftime('%I:%M:%S %p or %H') + '\t' + message
+    print '\n'
+    if manual:
+        section = getInt('Today\'s hours: ')
+        hours += section
+        dates[date].append(section)
+    else:
+        hours += delta
+        dates[date].append(delta)
+    os.system('clear')
+
+os.system('clear')
+print '%r total hours!' % hours
+for date in dates:
+    section = dates[date][-1]
+    print date, '\t%r hours' % section
